42dd2535c85c3ec016bfc561eb724583e1090303
Max Koval <max.koval.1008@gmail.com>
2021-04-18 00:58:38 +0300

    Dix statistics view

--- backend/pos/models/order_item.py
+++ backend/pos/models/order_item.py
@@ -1,20 +1,27 @@
 from django.db import models
 from django.utils import timezone
 from .product import Product
-from .order import Order
 
 
 class OrderItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_item')
-    start_time = models.DateTimeField(default=timezone.now, blank=True)
-    count = models.PositiveIntegerField(default=1)
-    total_price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey('Order', on_delete=models.CASCADE, related_name='order_item')
+    start_time = models.DateTimeField(default=timezone.now,blank=True)
+    product_amount = models.PositiveIntegerField(default=1)
+    total_price = models.DecimalField(max_digits=7, decimal_places=2,default=0)
     is_ready = models.BooleanField(default=None, blank=True, null=True)
+
+    def str(self):
+        return f'{self.pk}'
 
     def save(self, *args, **kwargs):
         product_cost = Product.objects.get(pk=self.product.id).price
-        self.total_price = product_cost * self.count
+
+        self.total_price = product_cost * self.product_amount
+        order = Order.objects.get(pk=self.order.id)
+        discount = Order.objects.get(pk=self.order.id).discount_choice
+        order.total_order_cost += self.total_price * Decimal(str(1-discount/100))
+        order.save()
         super(OrderItem, self).save(*args, **kwargs)
 
     def __str__(self):
