67610fd1b8405f14790974b9bd489784886f4410
Anton Kiryakov <anton_kiryakov2@mail.ru>
2021-04-18 10:01:45 +0300

    Deleted test views from pos app. Branch prepared for merge

--- backend/pos/models/table.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Table(models.Model):
-    is_active = models.BooleanField(default=True)
-    comment = models.TimeField(max_length=255, blank=True, null=True)
-
-    def __str__(self):
-        return f'Table №{self.pk}'
